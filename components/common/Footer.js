f749c7548ed7b0b490cc3a536d100713ec71d5bd
vasemkin <vasemkin@ya.ru>
2023-02-13 00:14:40 +0300

    linter

--- components/common/Footer.js
+++ components/common/Footer.js
@@ -4,52 +4,52 @@ import { GithubIcon } from './icons/GithubIcon'
 import { TgIcon } from './icons/TgIcon'
 
 const links = [
-    {
-        item: <>Documentation</>,
-        link: 'https://github.com/re-doubt/.github/blob/main/profile/README.MD',
-    },
-    {
-        item: <>Launch app</>,
-        link: 'https://beta.redoubt.online',
-    },
-    {
-        item: (
-            <GithubIcon color="var(--chakra-colors-black)" w="30px" h="30px" />
-        ),
-        link: 'https://github.com/re-doubt/',
-    },
-    {
-        item: <TgIcon color="var(--chakra-colors-black)" w="30px" h="30px" />,
-        link: 'https://t.me/re_doubt',
-    },
+	{
+		item: <>Documentation</>,
+		link: 'https://github.com/re-doubt/.github/blob/main/profile/README.MD'
+	},
+	{
+		item: <>Launch app</>,
+		link: 'https://beta.redoubt.online'
+	},
+	{
+		item: (
+			<GithubIcon color="var(--chakra-colors-black)" w="30px" h="30px" />
+		),
+		link: 'https://github.com/re-doubt/'
+	},
+	{
+		item: <TgIcon color="var(--chakra-colors-black)" w="30px" h="30px" />,
+		link: 'https://t.me/re_doubt'
+	}
 ]
 
 export const Footer = ({ ...rest }) => {
-    return (
-        <Box
-            display="flex"
-            justifyContent="flex-end"
-            alignItems="center"
-            {...rest}
-        >
-            <Grid
-                display="grid"
-                gridTemplateColumns={`repeat(${links.length},max-content)`}
-                gridGap={margins}
-            >
-                {links.map(({ item, link }, i) => (
-                    <Box
-                        display="flex"
-                        justifyContent="center"
-                        alignItems="center"
-                        key={`link-${i}`}
-                    >
-                        <Link href={link} target="_blank">
-                            {item}
-                        </Link>
-                    </Box>
-                ))}
-            </Grid>
-        </Box>
-    )
+	return (
+		<Box
+			display="flex"
+			justifyContent="flex-end"
+			alignItems="center"
+			{...rest}
+		>
+			<Grid
+				display="grid"
+				gridTemplateColumns={`repeat(${links.length},max-content)`}
+				gridGap={margins}
+			>
+				{links.map(({ item, link }, i) => (
+					<Box
+						display="flex"
+						justifyContent="center"
+						alignItems="center"
+						key={`link-${i}`}
+					>
+						<Link href={link} target="_blank">
+							{item}
+						</Link>
+					</Box>
+				))}
+			</Grid>
+		</Box>
+	)
 }
--- components/common/Jetton.js
+++ components/common/Jetton.js
@@ -7,97 +7,97 @@ const imageSize = '36px'
 const smallText = { base: '12px', md: '14px', lg: '18px' }
 
 const Property = ({ name, value, change, tonPrice, ...rest }) => {
-    const tonP = parseFloat(tonPrice)
+	const tonP = parseFloat(tonPrice)
 
-    const getValue = (name) => {
-        switch (name) {
-            case 'Price':
-                return `$${(value * tonP).toFixed(2)}`
-            case 'Volume':
-                return `$${parseInt(value * tonP, 10)}`
+	const getValue = (name) => {
+		switch (name) {
+		case 'Price':
+			return `$${(value * tonP).toFixed(2)}`
+		case 'Volume':
+			return `$${parseInt(value * tonP, 10)}`
 
-            default:
-                return value
-        }
-    }
+		default:
+			return value
+		}
+	}
 
-    const valueF = getValue(name)
-    const color =
+	const valueF = getValue(name)
+	const color =
         change > 0 ? 'var(--chakra-colors-green)' : 'var(--chakra-colors-red)'
 
-    return (
-        <Box
-            display="flex"
-            justifyContent="flex-end"
-            alignItems="center"
-            {...rest}
-        >
-            <Text fontSize={smallText} fontWeight={600}>
-                {name}:&nbsp;
-                {valueF}
-            </Text>
-            <Box
-                bg={color}
-                ml="8px"
-                borderRadius={borderRadius}
-                fontSize={smallText}
-                p="2px 6px"
-            >
-                <Text color="white" fontWeight={600}>
-                    {change}%
-                </Text>
-            </Box>
-        </Box>
-    )
+	return (
+		<Box
+			display="flex"
+			justifyContent="flex-end"
+			alignItems="center"
+			{...rest}
+		>
+			<Text fontSize={smallText} fontWeight={600}>
+				{name}:&nbsp;
+				{valueF}
+			</Text>
+			<Box
+				bg={color}
+				ml="8px"
+				borderRadius={borderRadius}
+				fontSize={smallText}
+				p="2px 6px"
+			>
+				<Text color="white" fontWeight={600}>
+					{change}%
+				</Text>
+			</Box>
+		</Box>
+	)
 }
 
 export const Jetton = ({ jetton, index, total, tonPrice, ...rest }) => {
-    const { address, name, price, marketVolume, totalHolders } = jetton
-    return (
-        <Box
-            p={margins}
-            border="2px solid var(--chakra-colors-blue)"
-            borderRadius={borderRadius}
-            boxShadow={boxShadow}
-            data-scroll
-            data-scroll-speed={total - index}
-            display="flex"
-            justifyContent="space-between"
-            alignItems="center"
-            flexDir="row"
-            {...rest}
-        >
-            <Box
-                display="flex"
-                justifyContent="flex-start"
-                alignItems="center"
-                flexDir="row"
-            >
-                <Image
-                    src={`${API_URL}v1/jettons/image/${address}`}
-                    w={imageSize}
-                    h={imageSize}
-                />
-                <Text ml="12px" fontWeight={600}>
-                    {name}
-                </Text>
-            </Box>
+	const { address, name, price, marketVolume, totalHolders } = jetton
+	return (
+		<Box
+			p={margins}
+			border="2px solid var(--chakra-colors-blue)"
+			borderRadius={borderRadius}
+			boxShadow={boxShadow}
+			data-scroll
+			data-scroll-speed={total - index}
+			display="flex"
+			justifyContent="space-between"
+			alignItems="center"
+			flexDir="row"
+			{...rest}
+		>
+			<Box
+				display="flex"
+				justifyContent="flex-start"
+				alignItems="center"
+				flexDir="row"
+			>
+				<Image
+					src={`${API_URL}v1/jettons/image/${address}`}
+					w={imageSize}
+					h={imageSize}
+				/>
+				<Text ml="12px" fontWeight={600}>
+					{name}
+				</Text>
+			</Box>
 
-            <Box display="grid" gridTemplate="1fr" gridGap="4px">
-                {[
-                    [price, 'Price'],
-                    [marketVolume, 'Volume'],
-                    [totalHolders, 'Holders'],
-                ].map(([prop, name], index) => (
-                    <Property
-                        name={name}
-                        value={prop.value}
-                        change={prop.percent}
-                        tonPrice={tonPrice}
-                        key={`${address}-${name}`}
-                    />
-                ))}
-            </Box>
-        </Box>
-    )
+			<Box display="grid" gridTemplate="1fr" gridGap="4px">
+				{[
+					[price, 'Price'],
+					[marketVolume, 'Volume'],
+					[totalHolders, 'Holders']
+				].map(([prop, name], index) => (
+					<Property
+						name={name}
+						value={prop.value}
+						change={prop.percent}
+						tonPrice={tonPrice}
+						key={`${address}-${name}`}
+					/>
+				))}
+			</Box>
+		</Box>
+	)
 }
